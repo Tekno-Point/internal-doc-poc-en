95fa80d6338fcc02d75d5c98d41e9574c0c18567
Om Gupta <om.gupta@teknopoint.in>
2025-02-14 19:13:35 +0530

    plugin added

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,15 +1,15 @@
 let popup;
 export function showMsg(status, message) {
-    popup.style.display = 'flex';
-    popup.querySelector('h2').textContent = message;
-    // if (status) {
-    // } else {
-    //     popup.style.display = 'none';
-    //     popup.querySelector('h2').textContent = message;
-    // }
+  popup.style.display = 'flex';
+  popup.querySelector('h2').textContent = message;
+  // if (status) {
+  // } else {
+  //     popup.style.display = 'none';
+  //     popup.querySelector('h2').textContent = message;
+  // }
 }
 export default function decorate(block) {
-    block.innerHTML = `
+  block.innerHTML = `
     <div>
         <div id="popup" class="popupnew">
             <div class="popup-content">
@@ -20,23 +20,23 @@ export default function decorate(block) {
         </div>
     </div>`;
 
-    popup = block.querySelector('.popupnew');
-    // const closePopupButton = block.querySelector('#closePopup');
+  popup = block.querySelector('.popupnew');
+  // const closePopupButton = block.querySelector('#closePopup');
 
-    // Show the popup
-    // document.addEventListener('click', () => {
-    //     popup.style.display = 'flex';
-    // });
+  // Show the popup
+  // document.addEventListener('click', () => {
+  //     popup.style.display = 'flex';
+  // });
 
-    // Close the popup
-    // closePopupButton.addEventListener('click', () => {
-    //     popup.style.display = 'none';
-    // });
+  // Close the popup
+  // closePopupButton.addEventListener('click', () => {
+  //     popup.style.display = 'none';
+  // });
 
-    // Close the popup if the user clicks outside the popup content
-    block.addEventListener('click', (e) => {
-        if (e.target === popup) {
-            popup.style.display = 'none';
-        }
-    });
+  // Close the popup if the user clicks outside the popup content
+  block.addEventListener('click', (e) => {
+    if (e.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
 }
