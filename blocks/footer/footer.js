9668df73d0ef9e43c99f47a5e6d83a40b96f15b0
Hrishikesh Shenoy <Hrishikesh.Shenoy@teknopoint.local>
2025-08-20 17:28:07 +0530

    .

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -17,4 +17,40 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+  document.querySelectorAll("li").forEach(li => {
+  if (li.textContent.includes("[pincode]")) {
+    const input = document.createElement("input");
+    input.type = "pincode";
+    input.placeholder = "Pincode";
+    input.style.width = "100%"; 
+    li.textContent = "";
+    li.appendChild(input);
+  }
+});
+document.querySelectorAll("li").forEach(li => {
+  if (li.textContent.includes("[Email Address]")) {
+    const input = document.createElement("input");
+    input.type = "email";
+    input.placeholder = "Add your email address here";
+    input.style.width = "100%"; 
+    li.textContent = "";
+    li.appendChild(input);
+  }
+});
+
+document.querySelectorAll("td p").forEach(p => {
+  p.addEventListener("click", function () {
+    const parentTd = this.closest("td");
+
+    // if clicked td is already active → remove it
+    if (parentTd.classList.contains("active")) {
+      parentTd.classList.remove("active");
+    } else {
+      // remove active from all td
+      document.querySelectorAll("td").forEach(td => td.classList.remove("active"));
+      // add active to clicked td
+      parentTd.classList.add("active");
+    }
+  });
+});
 }
